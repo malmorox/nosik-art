c6deb5b80c0f65ce6f000402088a9d3ab0bea100
Marcos Almorox <73890028+malmorox@users.noreply.github.com>
2023-11-03 20:45:16 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,130 +1,84 @@
-/* CARRITO DE COMPRA */
+// Tablero de juego
+var container = document.getElementById("container");
+container.innerHTML = "";
+var numberOfRows = 8;
+var numberOfColumns = 8;
+const ARRAY_DIFFERENCE = 1;
+var containerArray = [];
+var rowArray = [];
+var acc = 0;
+for (let r = 0; r < numberOfRows; r++) {
+    for (let c = 0; c < numberOfColumns; c++) {
+        rowArray[c] = `<div class="field white" id="number-${acc + ARRAY_DIFFERENCE}"></div>`;
+        container.innerHTML += rowArray[c];
+        acc++;
+    }
+    containerArray[r] = rowArray;
+    rowArray = [];
+}
 
-const btnCart = document.querySelector('.container-cart-icon');
-const containerCartProducts = document.querySelector(
-    '.container-cart-products'
-);
+var acc_player = 0;
+var numberOfColumn;
 
-btnCart.addEventListener('click', () => {
-    containerCartProducts.classList.toggle('hidden-cart');
-});
-
-/* ========================= */
-const cartInfo = document.querySelector('.cart-product');
-const rowProduct = document.querySelector('.row-product');
-
-// Lista de todos los contenedores de productos
-const productsList = document.querySelector('.container-items');
-
-// Variable de arreglos de Productos
-let allProducts = [];
-
-const valorTotal = document.querySelector('.total-pagar');
-
-const countProducts = document.querySelector('#contador-productos');
-
-const cartEmpty = document.querySelector('.cart-empty');
-const cartTotal = document.querySelector('.cart-total');
-
-productsList.addEventListener('click', e => {
-    if (e.target.classList.contains('btn-add-cart')) {
-        const product = e.target.parentElement;
-
-        const infoProduct = {
-            quantity: 1,
-            title: product.querySelector('h2').textContent,
-            price: product.querySelector('p').textContent,
-        };
-
-        const exits = allProducts.some(
-            product => product.title === infoProduct.title
-        );
-
-        if (exits) {
-            const products = allProducts.map(product => {
-                if (product.title === infoProduct.title) {
-                    product.quantity++;
-                    return product;
-                } else {
-                    return product;
-                }
-            });
-            allProducts = [...products];
-        } else {
-            allProducts = [...allProducts, infoProduct];
+container.addEventListener("click", (event) => {
+    var field = document.getElementById(event.target.id);
+    var idField = event.target.id;
+    if(field.className === "field white") {
+        if(columnIsEmpty(idField)) {
+            insertGamePiece(idField);
         }
-
-        showHTML();
     }
 });
 
-rowProduct.addEventListener('click', e => {
-    if (e.target.classList.contains('icon-close')) {
-        const product = e.target.parentElement;
-        const title = product.querySelector('p').textContent;
-
-        allProducts = allProducts.filter(
-            product => product.title !== title
-        );
-
-        console.log(allProducts);
-
-        showHTML();
+function getColumn(idField) {
+    var stringField = `<div class="field white" id="${idField}"></div>`;
+    for (let r = 0; r < numberOfRows; r++) {
+        for (let c = 0; c < numberOfColumns; c++) {
+            if(containerArray[r][c] === stringField) {
+                numberOfColumn = c;
+            }
+        }
     }
-});
+    return numberOfColumn;
+}
 
-// Funcion para mostrar  HTML
-const showHTML = () => {
-    if (!allProducts.length) {
-        cartEmpty.classList.remove('hidden');
-        rowProduct.classList.add('hidden');
-        cartTotal.classList.add('hidden');
-    } else {
-        cartEmpty.classList.add('hidden');
-        rowProduct.classList.remove('hidden');
-        cartTotal.classList.remove('hidden');
+function getWhiteFields(numberOfColumn) {
+    var numberOfId = numberOfColumn + 1;
+    var difference = 8;
+    var accColumn = 0;
+    for(let r = 0; r < numberOfRows; r++) {
+        if(containerArray[r][numberOfColumn] === `<div class="field white" id="number-${numberOfId}"></div>`) {
+            accColumn++;
+        }
+        numberOfId += difference;
     }
+    return accColumn;
+}
 
-    // Limpiar HTML
-    rowProduct.innerHTML = '';
+function columnIsEmpty(idField) {
+    var numberOfColumn = getColumn(idField);
+    console.log(numberOfColumn);
+    console.log(getWhiteFields(numberOfColumn));
+    return getWhiteFields(numberOfColumn) > 0 ? true : false;
+}
 
-    let total = 0;
-    let totalOfProducts = 0;
+var playerCount = 0;
 
-    allProducts.forEach(product => {
-        const containerProduct = document.createElement('div');
-        containerProduct.classList.add('cart-product');
+function insertGamePiece(idField) {
+    var numberOfColumn = getColumn(idField);
+    console.log(numberOfColumn);
+    var numberOfWhiteFields = getWhiteFields(numberOfColumn);
+    console.log(numberOfWhiteFields);
+    var idNumber = (numberOfColumn + ARRAY_DIFFERENCE) * numberOfWhiteFields;
+    console.log(idNumber);
+    if(playerCount % 2 === 0) {
+        containerArray[numberOfWhiteFields - ARRAY_DIFFERENCE][numberOfColumn] = `<div class="field red" id="${idNumber}"></div>`;
+        
+        reloadField(idField);
+    }
+}
 
-        containerProduct.innerHTML = `
-            <div class="info-cart-product">
-                <span class="cantidad-producto-carrito">${product.quantity}</span>
-                <p class="titulo-producto-carrito">${product.title}</p>
-                <span class="precio-producto-carrito">${product.price}</span>
-            </div>
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="icon-close"
-            >
-                <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M6 18L18 6M6 6l12 12"
-                />
-            </svg>
-        `;
-
-        rowProduct.append(containerProduct);
-
-        total =
-            total + parseInt(product.quantity * product.price.slice(1));
-        totalOfProducts = totalOfProducts + product.quantity;
-    });
-
-    valorTotal.innerText = `$${total}`;
-    countProducts.innerText = totalOfProducts;
-};
-
+function reloadField(idField) {
+    var field = document.getElementById(idField);
+    field.classList.replace("white", "red");
+}
