711612a5d1f30bf2143d78eed7a2d895d59ed7a9
malmorox <marcos.almorox@gmail.com>
2023-12-01 01:26:08 +0100

    Arreglando el redireccionamiendo y el carrito

--- js/gallery-functionality.js
+++ js/gallery-functionality.js
@@ -104,28 +104,3 @@ function redirectToArtPieceDetails(name, year, description, price, image) {
     let url = `art-piece.html?name=${name}&year=${year}&description=${description}&price=${price}&image=${image}`;
     window.location.href = url;
 }
-
-document.addEventListener('DOMContentLoaded', function () {
-    fillInfoInToDetails();
-});
-
-const ART_PIECE_DETAILS = document.querySelector('.nosik-art-piece');
-
-/* DETALLE DE OBRA - RELLENAR CAMPOS */
-
-//función para rellenar en la página de detalles de la obra
-function fillInfoInToDetails() {
-    const artPieceName = document.querySelector('.art-piece-info #name');
-    const artPieceYear = ART_PIECE_DETAILS.querySelector('.art-piece-info #year');
-    const artPieceDescription = ART_PIECE_DETAILS.querySelector('.art-piece-info #description');
-    const artPiecePrice = ART_PIECE_DETAILS.querySelector('.art-piece-info #price');
-    const artPieceImage = ART_PIECE_DETAILS.querySelector('.art-piece-info #image');
-    //asigna los valores del cuadro de la pagina 'Galería' en la página de detalles de la obra
-    console.log('fillInfoInToDetails is running');
-    let params = new URLSearchParams(window.location.search);
-    artPieceName.textContent = decodeURIComponent(params.get('name'));
-    artPieceYear.textContent = decodeURIComponent(params.get('year'));
-    artPieceDescription.textContent = decodeURIComponent(params.get('description'));
-    artPiecePrice.textContent = decodeURIComponent(params.get('price'));
-    artPieceImage.src = decodeURIComponent(params.get('image'));
-}
